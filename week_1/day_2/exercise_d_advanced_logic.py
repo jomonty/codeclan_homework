c7ba478b2e9bc9d818016332f82cf72a4a320c0d
jomonty <jomonty_cs@outlook.com>
2022-11-09 10:11:14 +0000

    updated variable in q1 ex d

--- week_1/day_2/exercise_d_advanced_logic.py
+++ week_1/day_2/exercise_d_advanced_logic.py
@@ -3,7 +3,7 @@
 numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
 
 # 1. Print out a list of the even integers:
-print([e for e in numbers if e % 2 == 0])
+print([num for num in numbers if num % 2 == 0])
 
 # 1. Without list comprehension
 even_nums = []

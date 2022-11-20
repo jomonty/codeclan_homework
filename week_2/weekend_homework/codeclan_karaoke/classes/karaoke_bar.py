9fd40985bdad689bf44bfb88d3cb00f3df7256ad
jomonty <100322748+jomonty@users.noreply.github.com>
2022-11-20 14:00:32 +0000

    added dictionary for room float rather than bar float

--- week_2/weekend_homework/codeclan_karaoke/classes/karaoke_bar.py
+++ week_2/weekend_homework/codeclan_karaoke/classes/karaoke_bar.py
@@ -9,8 +9,11 @@ class KaraokeBar:
         self.float = float
         self.room_charge = 5
         
-    def check_float(self):
-        return self.float
+    def check_total_float(self):
+        return sum([value for value in self.float.values()])
+    
+    def check_room_float(self, room):
+        return self.float[room]
     
     def find_room_by_name(self, name):
         for room in self.rooms:
@@ -18,15 +21,15 @@ class KaraokeBar:
                 return room
         return None
     
-    def add_to_float(self, amount):
-        self.float += amount
+    def add_to_room_float(self, room, amount):
+        self.float[room] += amount
     
     def add_guest_to_room(self, guest, room):
         if not room.is_room_full() and not room.is_guest_in_room(guest):
             if guest.can_afford(self.room_charge):
                 room.add_guest(guest)
                 guest.reduce_wallet(self.room_charge)
-                self.add_to_float(self.room_charge)
+                self.add_to_room_float(room, self.room_charge)
                 return guest.is_favourite_song(room.playlist)
     
     def remove_guest_from_room(self, guest, room):
--- week_2/weekend_homework/codeclan_karaoke/tests/karaoke_bar_tests.py
+++ week_2/weekend_homework/codeclan_karaoke/tests/karaoke_bar_tests.py
@@ -16,7 +16,8 @@ class TestKaraokeBar(unittest.TestCase):
         self.test_room_1 = Room('Skyfall', self.test_songs, 5)
         self.test_room_2 = Room('Crab Key', self.test_songs, 2)
         self.test_rooms = [self.test_room_1, self.test_room_2]
-        self.test_karaoke_bar_1 = KaraokeBar('Spectre', self.test_rooms, 100)
+        self.test_float = {self.test_room_1: 70, self.test_room_2: 30}
+        self.test_karaoke_bar_1 = KaraokeBar('Spectre', self.test_rooms, self.test_float)
         
     # @unittest.skip('skipped')
     def test_is_karaoke_bar(self):
@@ -26,8 +27,17 @@ class TestKaraokeBar(unittest.TestCase):
         self.assertEqual(self.test_karaoke_bar_1.room_charge, 5)
         
     # @unittest.skip('skipped')
-    def test_check_float(self):
-        self.assertEqual(100, self.test_karaoke_bar_1.float)
+    def test_check_total_float(self):
+        expected_output = 100
+        actual_output = self.test_karaoke_bar_1.check_total_float()
+        self.assertEqual(expected_output, actual_output)
+        
+    # @unittest.skip('skipped')
+    def test_check_room_float(self):
+        expected_output = 70
+        test_room = self.test_karaoke_bar_1.rooms[0]
+        actual_output = self.test_karaoke_bar_1.check_room_float(test_room)
+        self.assertEqual(expected_output, actual_output)
         
     # @unittest.skip('skipped')
     def test_find_room_by_name_pass(self):
@@ -43,16 +53,16 @@ class TestKaraokeBar(unittest.TestCase):
         
     # @unittest.skip('skipped')
     def test_add_to_float(self):
-        expected_output = 105
-        self.test_karaoke_bar_1.add_to_float(self.test_karaoke_bar_1.room_charge)
-        actual_output = self.test_karaoke_bar_1.float
+        expected_output = 75
+        self.test_karaoke_bar_1.add_to_room_float(self.test_room_1, self.test_karaoke_bar_1.room_charge)
+        actual_output = self.test_karaoke_bar_1.float[self.test_room_1]
         self.assertEqual(expected_output, actual_output)
         
     # @unittest.skip('skipped')
     def test_add_guest_to_room_pass_float(self):
-        expected_output = 105
+        expected_output = 75
         self.test_karaoke_bar_1.add_guest_to_room(self.test_guest_1, self.test_room_1)
-        actual_output = self.test_karaoke_bar_1.float
+        actual_output = self.test_karaoke_bar_1.float[self.test_room_1]
         self.assertEqual(expected_output, actual_output)
         
     # @unittest.skip('skipped')
@@ -71,17 +81,17 @@ class TestKaraokeBar(unittest.TestCase):
         
     # @unittest.skip('skipped')
     def test_add_guest_to_room_fail_cannot_afford_float(self):
-        expected_output = 100
+        expected_output = 70
         self.test_karaoke_bar_1.add_guest_to_room(self.test_guest_3, self.test_room_1)
-        actual_output = self.test_karaoke_bar_1.float
+        actual_output = self.test_karaoke_bar_1.float[self.test_room_1]
         self.assertEqual(expected_output, actual_output)
         
     # @unittest.skip('skipped')
     def test_add_guest_to_room_fail_room_full_float(self):
-        expected_output = 100
+        expected_output = 30
         self.test_room_2.guests = [self.test_guest_1, self.test_guest_2]
         self.test_karaoke_bar_1.add_guest_to_room(self.test_guest_3, self.test_room_2)
-        actual_output = self.test_karaoke_bar_1.float
+        actual_output = self.test_karaoke_bar_1.float[self.test_room_2]
         self.assertEqual(expected_output, actual_output)
         
     # @unittest.skip('skipped')

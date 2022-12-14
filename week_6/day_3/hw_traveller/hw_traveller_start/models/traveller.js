a95d0251a6cc59479973ddebdc72231b48c65026
jomonty <100322748+jomonty@users.noreply.github.com>
2022-12-14 19:42:13 +0000

    week_6/day_3 mvp homework complete

--- week_6/day_3/hw_traveller/hw_traveller_start/models/traveller.js
+++ week_6/day_3/hw_traveller/hw_traveller_start/models/traveller.js
@@ -3,28 +3,38 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(journey => journey.startLocation);
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map(journey => journey.endLocation);
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport);
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance >= minDistance);
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce(
+    (total, journey) => total + journey.distance, 0
+  );
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+// Less readable but works
+  // return this.journeys.map(journey => journey.transport).filter((transport, index, self) => {
+  //     return self.indexOf(transport) === index;
+  //   }
+  // );
+  const transportArray = this.journeys.map(journey => journey.transport);
 
+  return transportArray.filter( (transport, index) => {
+    return transportArray.indexOf(transport) === index;
+  });
 };
-
 
 module.exports = Traveller;

b06d65113a9cb6df24a648f12729e05089958be3
jomonty <100322748+jomonty@users.noreply.github.com>
2023-02-13 15:55:22 +0000

    finished advanced extensions

--- week_13/day_1/ThemeParkHomework/src/main/java/ThemePark.java
+++ week_13/day_1/ThemeParkHomework/src/main/java/ThemePark.java
@@ -1,9 +1,11 @@
 import attractions.Attraction;
 import behaviours.IReviewed;
+import behaviours.ISecurity;
 import stalls.Stall;
 import people.Visitor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ThemePark {
     // Initialisation
@@ -15,7 +17,7 @@ public class ThemePark {
         this.stalls = stalls;
     }
     // Getters and setters
-    
+
     // Methods
     public ArrayList<IReviewed> getAllReviewed() {
         ArrayList<IReviewed> reviewed = new ArrayList<>();
@@ -26,5 +28,27 @@ public class ThemePark {
     public void visit(Visitor visitor, Attraction attraction) {
         visitor.addVisitedAttraction(attraction);
         attraction.incrementVisitCount(1);
+    }
+    public HashMap<String, Integer> getAllRatings() {
+        HashMap<String, Integer> output = new HashMap<>();
+        ArrayList<IReviewed> reviewed = getAllReviewed();
+        for (IReviewed e : reviewed) {
+            output.put(e.getName(), e.getRating());
+        }
+        return output;
+    }
+    public ArrayList<IReviewed> getAllowedIReviewed(Visitor visitor) {
+        ArrayList<IReviewed> all = getAllReviewed();
+        ArrayList<IReviewed> output = new ArrayList<>();
+        for (IReviewed e : all) {
+            if (e instanceof ISecurity ) {
+                if (((ISecurity) e).isAllowedTo(visitor)) {
+                    output.add(e);
+                }
+            } else {
+                output.add(e);
+            }
+        }
+        return output;
     }
 }
--- week_13/day_1/ThemeParkHomework/src/test/java/ThemeParkTest.java
+++ week_13/day_1/ThemeParkHomework/src/test/java/ThemeParkTest.java
@@ -1,13 +1,11 @@
-import attractions.Attraction;
-import attractions.Rollercoaster;
+import attractions.*;
 import org.junit.Test;
-import stalls.IceCreamStall;
-import stalls.ParkingSpot;
-import stalls.Stall;
+import stalls.*;
 import people.Visitor;
 import org.junit.Before;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,20 +13,26 @@ public class ThemeParkTest {
     private ThemePark tp;
     private Rollercoaster rc1;
     private Visitor visitor1;
+    private Visitor visitor2;
     @Before
     public void setup() {
         ArrayList<Attraction> attractions = new ArrayList<>();
         ArrayList<Stall> stalls = new ArrayList<>();
         rc1 = new Rollercoaster("RC", 5);
         attractions.add(rc1);
-        IceCreamStall ic1 = new IceCreamStall("IC", "Mr Freeze", 1, ParkingSpot.B1);
-        stalls.add(ic1);
+        attractions.add(new Dodgems("Dodgems", 6));
+        attractions.add(new Park("Park", 7));
+        attractions.add(new Playground("Playground", 8));
+        stalls.add(new CandyFlossStall("CandyFlossStall", "CF Owner", 1, ParkingSpot.A1));
+        stalls.add(new IceCreamStall("IceCreamStall", "IC Owner", 2, ParkingSpot.A2));
+        stalls.add(new TobaccoStall("TobaccoStall", "TB Owner", 3, ParkingSpot.A3));
         visitor1 = new Visitor(20, 180, 200);
+        visitor2 = new Visitor(13, 120, 200);
         tp = new ThemePark(attractions, stalls);
     }
     @Test
     public void canGetAllIReviewed() {
-        assertEquals(2, tp.getAllReviewed().size());
+        assertEquals(7, tp.getAllReviewed().size());
     }
     @Test
     public void canVisit() {
@@ -38,5 +42,17 @@ public class ThemeParkTest {
         assertEquals(1, visitor1.getVisitedAttractions().size());
         assertEquals(1, rc1.getVisitCount());
     }
-
+    @Test
+    public void canGetAllRatings() {
+        HashMap<String, Integer> output = tp.getAllRatings();
+        int rcRating = output.get("RC");
+        int icRating = output.get("CandyFlossStall");
+        assertEquals(5, rcRating);
+        assertEquals(1, icRating);
+    }
+    @Test
+    public void canGetAllowedIReviewed() {
+        assertEquals(6, tp.getAllowedIReviewed(visitor1).size());
+        assertEquals(5, tp.getAllowedIReviewed(visitor2).size());
+    }
 }

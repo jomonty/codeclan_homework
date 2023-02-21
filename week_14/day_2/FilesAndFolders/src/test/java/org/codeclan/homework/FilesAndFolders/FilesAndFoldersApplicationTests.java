b42f220f4b490984b38cc5cffdf308d347d9d174
jomonty <100322748+jomonty@users.noreply.github.com>
2023-02-21 15:15:49 +0000

    homework template add

---
+++ week_14/day_2/FilesAndFolders/src/test/java/org/codeclan/homework/FilesAndFolders/FilesAndFoldersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codeclan.homework.FilesAndFolders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesAndFoldersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

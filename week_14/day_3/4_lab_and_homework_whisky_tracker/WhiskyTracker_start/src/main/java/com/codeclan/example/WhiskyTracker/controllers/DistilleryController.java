d062a42f88d6ead9537b983322e50c408623beb6
jomonty <100322748+jomonty@users.noreply.github.com>
2023-02-22 18:11:13 +0000

    homework mvp and extensions complete

--- week_14/day_3/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ week_14/day_3/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,10 +1,46 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class DistilleryController {
 
+    @Autowired
+    private DistilleryRepository distRepo;
+
+    @GetMapping(value = "/distilleries")
+    public ResponseEntity<List<Distillery>> getDistilleries(
+            @RequestParam Optional<String> region,
+            @RequestParam Optional<Integer> age
+    ) {
+        return region.map(s -> new ResponseEntity<>(
+                distRepo.findByRegionEquals(s),
+                HttpStatus.OK)
+        ).orElseGet(() -> age.map(integer -> new ResponseEntity<>(
+                distRepo.findByWhiskiesAgeEquals(integer),
+                HttpStatus.OK)
+        ).orElseGet(() -> new ResponseEntity<>(
+                distRepo.findAll(),
+                HttpStatus.OK
+        )));
+    }
+
+    @GetMapping(value = "/distilleries/{id}")
+    public ResponseEntity<Optional<Distillery>> getSingleDistillery(
+            @PathVariable long id
+    ) {
+        return new ResponseEntity<>(
+                distRepo.findById(id),
+                HttpStatus.OK
+        );
+    }
 
 }
--- week_14/day_3/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ week_14/day_3/4_lab_and_homework_whisky_tracker/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,9 +1,54 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class WhiskyController {
+
+    @Autowired
+    private WhiskyRepository whiskyRepo;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getWhiskies(
+            @RequestParam Map<String, String> allParams
+            ) {
+        List<Whisky> whiskies = new ArrayList<>();
+        if (allParams.size() == 0) {
+            whiskies = whiskyRepo.findAll();
+        } else if (allParams.size() == 1 && allParams.containsKey("year")) {
+            whiskies = whiskyRepo.findByYearEquals(Integer.parseInt(allParams.get("year")));
+        } else if (allParams.size() == 1 && allParams.containsKey("distilleryRegion")) {
+            whiskies = whiskyRepo.findByDistilleryRegion(allParams.get("distilleryRegion"));
+        } else if (allParams.size() == 2 && allParams.containsKey("distilleryName") && allParams.containsKey("age")) {
+            whiskies = whiskyRepo.findByAgeAndDistilleryName(
+                    Integer.parseInt(allParams.get("age")),
+                    allParams.get("distilleryName")
+            );
+        }
+        return new ResponseEntity<>(
+                whiskies,
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping(value = "/whiskies/{id}")
+    public ResponseEntity<Optional<Whisky>> getSingleWhisky(
+            @PathVariable long id
+    ) {
+        return new ResponseEntity<>(
+                whiskyRepo.findById(id),
+                HttpStatus.OK
+        );
+    }
 
 }
